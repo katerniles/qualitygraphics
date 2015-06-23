14e61e2c58cf068f57c6b2fa279f73fad28c854b
Kate Russell <krussellnd@gmail.com>
2015-06-22 22:17:55 -0400

    confusion

--- db/migrate/20150616002739_create_products.rb
+++ db/migrate/20150616002739_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.text :description
+      t.text :descxription
       t.string :image_url
 
       t.timestamps null: false
